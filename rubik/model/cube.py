9bbfdd9f7d04ac9340737e07291cb2a60fd82924
Colby Deason <colbydeason@gmail.com>
2023-02-13 00:50:36 -0600

    Green Light    |

--- rubik/model/cube.py
+++ rubik/model/cube.py
@@ -72,6 +72,7 @@ class Cube:
                 for j in range(len(uniqueArray)):
                     if self._cube[i] == uniqueArray[j][0]:
                         uniqueArray[j][1] = uniqueArray[j][1] +"1"
+                        continue
                     elif j == len(uniqueArray) - 1:
                         uniqueArray.append([self._cube[i], "1"])    
         print(uniqueArray)
