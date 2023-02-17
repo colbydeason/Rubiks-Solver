a371064fc5a6d566548941aebb3ef25584487aa5
Colby Deason <colbydeason@gmail.com>
2023-02-17 13:58:02 -0600

    Green Light    |

--- rubik/controller/bottomCross.py
+++ rubik/controller/bottomCross.py
@@ -9,11 +9,32 @@ def solveBottomCross(theCube: Cube) -> str:
     #     a) check each side and move white edge pieces to the top to form the flower
     # Step 2: Solve cross
     #     b) match up edge to face center and rotate side 
-    def searchEdges(color, searchCube):
-        firstEdge = None
-        edgeList = {}
-        return firstEdge
     
+    edgeList = {'1', '3', '5', '7', '46'}
+    bottomColor = theCube.get()[49]
+    for i in range(4):
+        topEdge = theCube.getRelativeSquare(43)
+        if topEdge == bottomColor:
+            theCube.rotate('U')
+            continue
+        else:
+            for j in range (4):
+                if edgeList[0] == bottomColor:
+                    theCube.rotate('FuRU')
+                    break
+                if edgeList[1] == bottomColor:
+                    theCube.rotate('Ulu')
+                    break
+                if edgeList[2] == bottomColor:
+                    theCube.rotate('uRU')
+                    break
+                if edgeList[3] == bottomColor:
+                    theCube.rotate('FUru')
+                    break
+                if edgeList[4] == bottomColor:
+                    theCube.rotate('FF')
+                    break
+        theCube.rotate('U')
     
     
     
