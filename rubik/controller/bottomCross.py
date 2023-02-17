1ac909fb04ce839f33c8d432584ff86126a858e2
Colby Deason <colbydeason@gmail.com>
2023-02-17 13:16:21 -0600

    Red Light      |

--- rubik/controller/bottomCross.py
+++ rubik/controller/bottomCross.py
@@ -1,4 +1,4 @@
-import rubik.model.constants
+from rubik.model.constants import *
 from rubik.model.cube import Cube
 
 def solveBottomCross(theCube: Cube) -> str:
@@ -9,8 +9,10 @@ def solveBottomCross(theCube: Cube) -> str:
     #     a) check each side and move white edge pieces to the top to form the flower
     # Step 2: Solve cross
     #     b) match up edge to face center and rotate side 
-    
-    
+    def searchEdges(color, searchCube):
+        firstEdge = None
+        edgeList = {}
+        return firstEdge
     
     
     
--- rubik/test/bottomCrossTest.py
+++ rubik/test/bottomCrossTest.py
@@ -4,12 +4,38 @@ Created on Feb 15, 2023
 @author: Colby
 '''
 import unittest
+import rubik.model.cube as cube
 from rubik.controller.bottomCross import solveBottomCross
 
 class Test(unittest.TestCase):
 
     # Analysis of solveBottomCross:
+    #     Solves the bottom cross of the cube, including matching edges
     #
-    # 
+    #     input:
+    #         theCube: Cube object
+    #
+    #     output: 
+    #         nominal:
+    #             bottomCrossString: string of the rotations that solved the bottom cross
+    #         abnormal:
+    #             none
+    #         side-effects:
+    #             Cube object is rotated and the bottom cross is solved
+    #       
+    #     happy path:
+    #         DEVELOPER TESTS
+    #         test 001: Flower stage
+    #         test 002: bottom cross
     
-    
+    def test_bottomCrossTest_solveBottomCross_001_TopFlower(self):
+        testCube = cube.Cube('114205020241014115213322235500133144004342453355054253')
+        bottomColor = testCube.get()[49]
+        edgeList = {37, 39, 41, 43}
+        solveBottomCross(testCube)
+        for edge in edgeList:
+            if testCube.get(edge) == bottomColor:
+                continue
+            else
+                self.fail()
+        pass
