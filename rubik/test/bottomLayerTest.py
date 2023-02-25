100d7197dce32f0f83474b740439dfc6cfcb768d
Colby Deason <colbydeason@gmail.com>
2023-02-24 19:49:44 -0600

    Red Light      |

--- rubik/test/bottomLayerTest.py
+++ rubik/test/bottomLayerTest.py
@@ -34,7 +34,7 @@ class Test(unittest.TestCase):
     #         test 006: bottom layer solved
     
     def test_bottomLayerTest_solveBottomCross_001_CornerOut(self):
-        testCube = cube.Cube('')
+        testCube = cube.Cube('220202332535115004202022011414433500411545341153454533')
         cornersCheckList = [[0, 42, 29], [2, 44, 9], [11, 18, 38], [36, 20, 27]]
         bottomColor = testCube.get()[49]
         for group in cornersCheckList:
@@ -48,7 +48,7 @@ class Test(unittest.TestCase):
                 
             
     def test_bottomLayerTest_solveBottomCross_002_BottomLayer(self):
-        testCube = cube.Cube('')
+        testCube = cube.Cube('302102343140113413101223014022535252554440435530250145')
         quadMatchList = [[4, 6, 7, 8], [13, 15, 16, 17], [22, 24, 25, 26], [31, 33, 34, 35]]
         bottomRangeList = {45, 46, 47, 48, 49, 50, 51, 52, 53}
         for group in quadMatchList:
@@ -68,7 +68,7 @@ class Test(unittest.TestCase):
             self.fail("Bottom face is not solid")
     
     def test_bottomLayerTest_solveBottomCross_003_BottomLayer(self):
-        testCube = cube.Cube('')
+        testCube = cube.Cube('120504224505110033223524531544331023051041241413350452')
         quadMatchList = [[4, 6, 7, 8], [13, 15, 16, 17], [22, 24, 25, 26], [31, 33, 34, 35]]
         bottomRangeList = {45, 46, 47, 48, 49, 50, 51, 52, 53}
         for group in quadMatchList:
@@ -88,7 +88,7 @@ class Test(unittest.TestCase):
             self.fail("Bottom face is not solid")
     
     def test_bottomLayerTest_solveBottomCross_004_BottomLayer(self):
-        testCube = cube.Cube('')
+        testCube = cube.Cube('402001044321211241030520252351433314555043014331452525')
         quadMatchList = [[4, 6, 7, 8], [13, 15, 16, 17], [22, 24, 25, 26], [31, 33, 34, 35]]
         bottomRangeList = {45, 46, 47, 48, 49, 50, 51, 52, 53}
         for group in quadMatchList:
@@ -108,7 +108,7 @@ class Test(unittest.TestCase):
             self.fail("Bottom face is not solid")
     
     def test_bottomLayerTest_solveBottomCross_005_BottomLayer(self):
-        testCube = cube.Cube('')
+        testCube = cube.Cube('135004211034115530241120340212435455433240553300252124')
         quadMatchList = [[4, 6, 7, 8], [13, 15, 16, 17], [22, 24, 25, 26], [31, 33, 34, 35]]
         bottomRangeList = {45, 46, 47, 48, 49, 50, 51, 52, 53}
         for group in quadMatchList:
@@ -128,7 +128,7 @@ class Test(unittest.TestCase):
             self.fail("Bottom face is not solid")
     
     def test_bottomLayerTest_solveBottomCross_006_BottomLayer(self):
-        testCube = cube.Cube('')
+        testCube = cube.Cube('000000000111111111222222222333333333444444444555555555')
         quadMatchList = [[4, 6, 7, 8], [13, 15, 16, 17], [22, 24, 25, 26], [31, 33, 34, 35]]
         bottomRangeList = {45, 46, 47, 48, 49, 50, 51, 52, 53}
         for group in quadMatchList:
