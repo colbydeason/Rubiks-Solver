a1f5fc63ce389d5275f0b866211979ded10598d0
Colby Deason <colbydeason@gmail.com>
2023-02-25 18:48:16 -0600

    Green Light    |

--- rubik/test/solveTest.py
+++ rubik/test/solveTest.py
@@ -21,11 +21,11 @@ class SolveTest(TestCase):
 #     happy path:
 #         test 000: continuous check test -> does string output equal consequence of cube state
 #         test 001: solve bottom cross
-#         test 002: solve bottom layer
-#         test 003: solve middle layer
-#         test 004: solve up face cross
-#         test 005: solve up face surface
-#         test 006: solve upper layer / full cube
+#         test 002-003: solve bottom layer
+#         test 004-005: solve middle layer
+#         test 006-007: solve up face cross
+#         test 009-009: solve up face surface
+#         test 010-011: solve upper layer / full cube
 #
 #     sad path:
 #         test 901: invalid cube (implementation only, boundary tests done by cubeTest for invalid cube)
@@ -63,6 +63,36 @@ class SolveTest(TestCase):
         parmsRotate= {}
         parmsSolve['cube'] = '453302001421512031240422212500134313114343302544055555'
         parmsRotate['cube'] = '453302001421512031240422212500134313114343302544055555'
+        solveDic = solve(parmsSolve)
+        solveString = solveDic['solution']
+        parmsRotate['dir'] = solveString
+        rotateDic = rotate(parmsRotate)
+        testCube = rotateDic['cube']
+        print(testCube + ',')
+        
+        quadMatchList = [[4, 6, 7, 8], [13, 15, 16, 17], [22, 24, 25, 26], [31, 33, 34, 35]]
+        bottomRangeList = {45, 46, 47, 48, 49, 50, 51, 52, 53}
+        for group in quadMatchList:
+            colorArray = []
+            for color in group:
+                colorArray.append(testCube[color])
+            if colorArray.count(colorArray[0]) == len(colorArray):
+                continue
+            else:
+                self.fail("Sides do not match color")
+        colorArray = []
+        for square in bottomRangeList:
+            colorArray.append(testCube[square])
+        if colorArray.count(colorArray[0]) == len(colorArray):
+            pass
+        else:
+            self.fail("Bottom face is not solid")
+            
+    def test_solveTest_solve_003_BottomLayer(self):
+        parmsSolve = {}
+        parmsRotate= {}
+        parmsSolve['cube'] = '113504143452313041513523442035030504423242052025151102'
+        parmsRotate['cube'] = '113504143452313041513523442035030504423242052025151102'
         solveDic = solve(parmsSolve)
         solveString = solveDic['solution']
         parmsRotate['dir'] = solveString
