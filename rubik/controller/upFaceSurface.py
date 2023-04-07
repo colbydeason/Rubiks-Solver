4df4444eabfa96a02ddca562002a32d756aac2f8
Colby Deason <colbydeason@gmail.com>
2023-04-07 17:37:27 -0500

    Green Light    |

--- rubik/controller/upFaceSurface.py
+++ rubik/controller/upFaceSurface.py
@@ -1,9 +1,26 @@
-import rubik.model.constants
+from rubik.model.constants import *
 from rubik.model.cube import Cube
 
 def solveUpSurface(theCube: Cube) -> str:
-    
+    cornerArray = [UTL, UTR, UBL, UBR]
+    cornerColorArray = []
+    topColor = theCube[UMM]
+    for corner in cornerArray:
+        cornerColorArray.append(theCube.getRelativeSquare(corner))
+    numberMatching = cornerColorArray.count(topColor)
+    while _alignCorner(theCube, numberMatching):
+        theCube.rotate("RUrURUUr")
     return theCube.popCurrentRotationStringResetOrientation()
 
-def _alignCorner(cornerCube):
-    return True
+def _alignCorner(cornerCube, numberMatching):
+    topColor = cornerCube[UMM]
+    if numberMatching == 0 or numberMatching == 2:
+        while topColor != cornerCube.getRelativeSquare(LTR):
+            cornerCube.rotateCubeR()
+        return True
+    elif numberMatching == 1:
+        while topColor != cornerCube.getRelativeSquare(UBL):
+            cornerCube.rotateCubeR()
+        return True
+    else:
+        return False
