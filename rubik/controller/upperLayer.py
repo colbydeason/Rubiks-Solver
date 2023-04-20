d54826fcecab15f2b6c7ae2d94297f6a0fea940c
Colby Deason <colbydeason@gmail.com>
2023-04-20 14:41:48 -0500

    Green Light    |

--- rubik/controller/upperLayer.py
+++ rubik/controller/upperLayer.py
@@ -1,4 +1,4 @@
-import rubik.model.constants
+from rubik.model.constants import *
 from rubik.model.cube import Cube
 
 def solveUpperLayer(theCube: Cube) -> str:
@@ -9,6 +9,22 @@ def solveUpperLayer(theCube: Cube) -> str:
     return theCube.popCurrentRotationStringResetOrientation()
 
 def _solveCorners(cornerCube):
+    for algo in range(2):
+        numSolved = 0
+        for face in range(4):
+            if cornerCube.isSameColor([FTL, FTR]):
+                numSolved += 1
+                cornerCube.rotateCubeR()
+        if numSolved == 4:
+            return
+        for face in range(3):
+            if cornerCube.isSameColor([FTL, FTR]):
+                cornerCube.rotateCubeR()
+                cornerCube.rotateCubeR()
+                break
+            else:
+                cornerCube.rotateCubeR()
+        cornerCube.rotate('rFrBBRfrBBRR')
     return 
 
 def _solveEdges(edgeCube):
